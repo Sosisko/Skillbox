447b1f5861a7775418055e71f7fd3a1d9877f437
Sosisko <54500448+Sosisko@users.noreply.github.com>
2020-04-25 20:55:25 +0500

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -1,47 +1,49 @@
-var modal = document.getElementsByClassName("overlay")[0];
-	callOrderBtn = document.getElementsByClassName("call-order");
-	knowMoreBtn = document.querySelectorAll(".know-more-btn");
-	btnClose = document.getElementsByClassName("popup__close_btn")[0];
-	blur = document.getElementsByClassName("blur")[0];
-	body = document.body;
-	email = document.getElementById("email");
+
+var modal = $('.overlay')[0];
+	callOrderBtn = $('.call-order');
+	knowMoreBtn = $('.know-more-btn');
+	btnClose = $('.popup__close_btn')[0];
+
+
+
 
 for (var i = 0; i < callOrderBtn.length; i++) {
   callOrderBtn[i].onclick = function(e){
-  	e.preventDefault();
-    modal.style.display = 'inline-block'; //Включает модальное окно
-	blur.style.filter = 'blur(3px)'; //Включаем размытие фона
-	body.style.overflow = 'hidden'; //Выключаем скролл для body
-	email.style.display = 'none'; // Скрывает поле email
+	e.preventDefault();
+	$('.overlay').css('display', 'inline-block'); //Включает модальное окно
+	$('.blur').css('filter','blur(3px)'); //Включаем размытие фона
+	$(document.body).css('overflow', 'hidden'); //Выключаем скролл для body
+	$('#email').css('display', 'none'); // Скрывает поле email
   };
 }
 
 for (var i = 0; i < knowMoreBtn.length; i++) {
   knowMoreBtn[i].onclick = function(e){
   	e.preventDefault();
-    modal.style.display = 'inline-block'; //Включает модальное окно
-	blur.style.filter = 'blur(3px)'; //Включаем размытие фона
-	body.style.overflow = 'hidden'; //Выключаем скролл для body
-	email.style.display = 'block'; //Показывает поле email
+	$('.overlay').css('display', 'inline-block'); //Включает модальное окно
+	$('.blur').css('filter','blur(3px)'); //Включаем размытие фона
+	$(document.body).css('overflow', 'hidden'); //Выключаем скролл для body
+	$('#email').css('display', 'block'); //Показывает поле email
   };
 }
 
 
 btnClose.onclick = function () {
-	modal.style.display = 'none'; //Выключает модальное окно по нажатию на кнопку Х
-	blur.style.filter = 'blur(0)'; //Выключаем размытие фона
-	body.style.overflow = 'visible'; //Включаем скролл для body
+	$('.overlay').css('display', 'none'); //Выключает модальное окно по нажатию на кнопку Х
+	$('.blur').css('filter','blur(0)'); //Выключаем размытие фона
+	$(document.body).css('overflow', 'visible'); //Включаем скролл для body
 
 };
+
 
 window.onclick = function (event) {
 	if (event.target == modal) {
-		modal.style.display = 'none'; //Выключает модальное окно по нажатию вне окна
-		blur.style.filter = 'blur(0)'; //Выключаем размытие фона
-		body.style.overflow = 'visible'; //Включаем скролл для body
+		$('.overlay').css('display', 'none'); //Выключает модальное окно по нажатию вне окна
+		$('.blur').css('filter','blur(0)'); //Выключаем размытие фона
+		$(document.body).css('overflow', 'visible'); //Включаем скролл для body
+		$('.wrapper__popup_sended').hide(); //Скрывает окно сообщения отправки
 	}
 };
-
 
 
 	function send(e){
@@ -60,11 +62,7 @@ window.onclick = function (event) {
 			text = $('#phone').val('');
 			text = $('#email').val('');
 
-			$('.overlay').append (`
-				<div class="wrapper__popup_sended">
-        			<p>Благодарим!</p>
-        			<button class="close-btn-ok">OK</button>
-       			</div>`)
+			$('.wrapper__popup_sended').show();
 
 			
 		} else{
@@ -72,23 +70,20 @@ window.onclick = function (event) {
 			$('#phone').addClass('error');
 			$('#email').addClass('error');
 		}
-
-			var btnCloseOk = document.getElementsByClassName("close-btn-ok")[0];
-			var popupSended = document.getElementsByClassName("wrapper__popup_sended")[0];
-
-			btnCloseOk.onclick = function () {
-				modal.style.display = 'none'; //Выключает модальное окно по нажатию на кнопку Х
-				blur.style.filter = 'blur(0)'; //Выключаем размытие фона
-				body.style.overflow = 'visible'; //Включаем скролл для body
-				$('.wrapper__popup_sended').hide(); //Скрывает окно сообщения отправки
-				};
-
-
 	}
+
 
    $('.popup__send-btn').on('click', send);
 
 
+var btnCloseOk = $('.close-btn-ok')[0];
+
+		btnCloseOk.onclick = function () {
+			$('.overlay').css('display', 'none'); //Выключает модальное окно по нажатию вне окна
+			$('.blur').css('filter','blur(0)'); //Выключаем размытие фона
+			$(document.body).css('overflow', 'visible'); //Включаем скролл для body
+			$('.wrapper__popup_sended').hide(); //Скрывает окно сообщения отправки
+		};
 
 
 	
